355a882d9cd75e0ed46c3c10bc5346246ffe80c3
Aman Binjola <abinjola@sparkcognition.com>
2023-10-26 00:53:51 +0530

    - Added prettier json - removed test files - made folder structure changes

---
+++ incense_frontend/src/shared/constants/colors.jsx
@@ -0,0 +1,7 @@
+export default colors = {
+  'cherry-blossom-pink': '#FDAEBAff',
+  'light-coral': '#E18080ff',
+  'salmon-pink': '#FE829Bff',
+  'salmon-pink-2': '#FEA5B2ff',
+  'lavender-blush': '#EDE6EAff',
+};
