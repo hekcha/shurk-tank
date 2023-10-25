5cb95bf90b2ca5d22602274ad63cea261d06926f
Aman Binjola <abinjola@sparkcognition.com>
2023-10-26 01:07:42 +0530

    default page

--- incense_frontend/src/App.js
+++ incense_frontend/src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import { color } from './shared/constants/colors';
 
 function App() {
-  return <div className="App">Shree Ansh Agarbatti</div>;
+  return (
+    <div
+      className="App flex justify-center items-center h-screen bg-slate-500"
+      style={{ color: color['cherry-blossom-pink'] }}
+    >
+      Shree Ansh Agarbatti
+    </div>
+  );
 }
 
 export default App;
--- incense_frontend/src/shared/constants/colors.jsx
+++ incense_frontend/src/shared/constants/colors.jsx
@@ -1,7 +1,9 @@
-export default colors = {
+export let color = {
   'cherry-blossom-pink': '#FDAEBAff',
   'light-coral': '#E18080ff',
   'salmon-pink': '#FE829Bff',
   'salmon-pink-2': '#FEA5B2ff',
   'lavender-blush': '#EDE6EAff',
 };
+
+export * as colors from './colors.jsx';
