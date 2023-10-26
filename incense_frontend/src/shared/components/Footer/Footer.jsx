d9e23d31df0c967a0d39eb55f10fc36c193620d4
Aman Binjola <abinjola@sparkcognition.com>
2023-10-27 00:41:31 +0530

    Added Footer and social media assets

--- incense_frontend/src/shared/components/Footer/Footer.jsx
+++ incense_frontend/src/shared/components/Footer/Footer.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './Footer.css';
+import logo from '../../../assets/logo-white.png';
+import instagram from '../../../assets/instagram.svg';
+import facebook from '../../../assets/facebook.svg';
+import twitter from '../../../assets/twitter.svg';
 
 export const Footer = () => {
   return (
-    <>
+    <div>
       <svg
         width="100%"
         height="100%"
         id="svg"
-        viewBox="0 0 1440 690"
+        viewBox="0 0 1440 590"
         xmlns="http://www.w3.org/2000/svg"
         className="transition duration-300 ease-in-out delay-150 "
       >
@@ -40,10 +44,163 @@ export const Footer = () => {
           fillOpacity="1"
           className="transition-all duration-300 ease-in-out delay-150 path-1"
         ></path>
-        <foreignObject x="20" y="90" width="150" height="200">
-          <p xmlns="http://www.w3.org/1999/xhtml">Footer</p>
-        </foreignObject>
       </svg>
-    </>
+
+      <footer
+        className="relative pt-8 pb-6"
+        style={{
+          backgroundImage: 'linear-gradient(0.25turn, #F78DA7 5%, #8ED1FC 95%)',
+        }}
+      >
+        <div className="container mx-auto px-4">
+          <div className="flex flex-wrap text-left lg:text-left">
+            <div className="w-full lg:w-6/12 px-4">
+              <div className='justify-start sm:pb-2.5 md:pb-5'>
+                <img
+                  src={logo}
+                  alt="company logo"
+                  style={{
+                    height: '120px',
+                  }}
+                />
+              </div>
+              {/* <h4 className="text-3xl fonat-semibold text-blueGray-700">
+                Let's keep in touch!
+              </h4> */}
+              <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
+                Find us on any of these platforms, we respond 1-2 business days.
+              </h5>
+              <div className="mt-6 lg:mb-0 mb-6">
+                <button
+                  className="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                  type="button"
+                >
+                  <span className="flex justify-center items-center">
+                    <img src={facebook} className="h-5" alt="social media" />
+                  </span>
+                </button>
+                <button
+                  className="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                  type="button"
+                >
+                  <span className="flex justify-center items-center">
+                    <img src={instagram} className="h-5" alt="social media" />
+                  </span>
+                </button>
+                <button
+                  className="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                  type="button"
+                >
+                  <span className="flex justify-center items-center">
+                    <img src={twitter} className="h-5" alt="social media" />
+                  </span>
+                </button>
+              </div>
+            </div>
+            <div className="w-full lg:w-6/12 px-4">
+              <div className="flex flex-wrap items-top mb-6">
+                <div className="w-full lg:w-4/12 px-4 ml-auto">
+                  <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
+                    Useful Links
+                  </span>
+                  <ul className="list-unstyled">
+                    <li>
+                      <a
+                        className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://www.creative-tim.com/presentation?ref=njs-profile"
+                      >
+                        About Us
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://blog.creative-tim.com?ref=njs-profile"
+                      >
+                        Blog
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://www.github.com/creativetimofficial?ref=njs-profile"
+                      >
+                        Github
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://www.creative-tim.com/bootstrap-themes/free?ref=njs-profile"
+                      >
+                        Free Products
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="w-full lg:w-4/12 px-4">
+                  <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
+                    Other Resources
+                  </span>
+                  <ul className="list-unstyled">
+                    <li>
+                      <a
+                        className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md?ref=njs-profile"
+                      >
+                        MIT License
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://creative-tim.com/terms?ref=njs-profile"
+                      >
+                        Terms &amp; Conditions
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://creative-tim.com/privacy?ref=njs-profile"
+                      >
+                        Privacy Policy
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://creative-tim.com/contact-us?ref=njs-profile"
+                      >
+                        Contact Us
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr className="my-6 border-blueGray-300" />
+          <div className="flex flex-wrap items-center md:justify-between justify-center">
+            <div className="w-full md:w-4/12 px-4 mx-auto text-center">
+              <div className="text-sm text-blueGray-500 font-semibold py-1">
+                Address:
+                <a
+                  href="https://maps.app.goo.gl/esvj5UkrWBXzgWoY6"
+                  className="text-blueGray-500 hover:text-gray-800"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {' '}
+                  Shree Ansh Agarbatti co. Gola Bazar, Gorakhpur, Uttar Pradesh,
+                  273408, India
+                </a>
+                .
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
   );
 };
