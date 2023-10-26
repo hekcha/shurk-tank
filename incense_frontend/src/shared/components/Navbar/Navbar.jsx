3e742a276255def3425f9a5841c96ff864ccfa76
Aman Binjola <abinjola@sparkcognition.com>
2023-10-26 10:16:41 +0530

    - Added svg files for navbar

--- incense_frontend/src/shared/components/Navbar/Navbar.jsx
+++ incense_frontend/src/shared/components/Navbar/Navbar.jsx
@@ -12,7 +12,34 @@ export const Navbar = ({ toggle }) => {
         })}
         <div className="icons">
           <div className="mobile-menu-icon">
-            <button onClick={toggle}>{'>'}</button>
+            <button onClick={toggle}>
+              <svg
+                width="30px"
+                height="30px"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M4 18L20 18"
+                  stroke="#ffffff"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                />
+                <path
+                  d="M4 12L20 12"
+                  stroke="#ffffff"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                />
+                <path
+                  d="M4 6L20 6"
+                  stroke="#ffffff"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                />
+              </svg>
+            </button>
           </div>
         </div>
       </div>
